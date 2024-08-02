769e18e7ee8e6f0587b43451ccfba1212a553199
yeggrrr <yjkim981103@gmail.com>
2024-08-02 17:06:17 +0900

    [Feat] NicknameVC - UI & BirthdayVC 연결

--- YegrRxThreads_Start/SignUpOptional/NicknameViewController.swift
+++ YegrRxThreads_Start/SignUpOptional/NicknameViewController.swift
@@ -6,12 +6,57 @@
 //
 
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
 
-final class NicknameViewController: UIViewController {
+final class NicknameViewController: UIViewController, ViewRepresentable {
+    let nicknameTextField = UITextField()
+    let nextButton = UIButton()
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        addSubviews()
+        setConstraints()
+        configureUI()
+        bind()
+    }
+    
+    func bind() {
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                owner.navigationController?.pushViewController(BirthdayViewController(), animated: true)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    func addSubviews() {
+        view.addSubviews([nicknameTextField, nextButton])
+    }
+    
+    func setConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        nicknameTextField.snp.makeConstraints {
+            $0.height.equalTo(50)
+            $0.top.equalTo(safeArea).offset(200)
+            $0.horizontalEdges.equalTo(safeArea).inset(20)
+        }
+        
+        nextButton.snp.makeConstraints {
+            $0.height.equalTo(50)
+            $0.top.equalTo(nicknameTextField.snp.bottom).offset(30)
+            $0.horizontalEdges.equalTo(safeArea).inset(20)
+        }
+    }
+    
+    func configureUI() {
         view.backgroundColor = .white
+        
+        nicknameTextField.setUI(placeholderText: "닉네임을 입력해주세요")
+        nextButton.setUI(title: "다음")
     }
 }
