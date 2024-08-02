21b2701c2eedaad76304fe964dd41d6d298b7099
yeggrrr <yjkim981103@gmail.com>
2024-08-02 19:37:33 +0900

    [Feat] BirthdayVC - UI & 선택한 date에 해당하는 dateLabel 띄우기

--- YegrRxThreads_Start/SignUpOptional/BirthdayViewController.swift
+++ YegrRxThreads_Start/SignUpOptional/BirthdayViewController.swift
@@ -6,12 +6,122 @@
 //
 
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
 
-final class BirthdayViewController: UIViewController {
+final class BirthdayViewController: UIViewController, ViewRepresentable {
+    let birthDayPicker = UIDatePicker()
+    let infoLabel = UILabel()
+    let containerStackView = UIStackView()
+    let yearLabel = UILabel()
+    let monthLabel = UILabel()
+    let dayLabel = UILabel()
+    let nextButton = UIButton()
+    
+    let year = BehaviorRelay(value: 2024)
+    let month = BehaviorRelay(value: 8)
+    let day = BehaviorRelay(value: 1)
+    
+    var differenceDay = BehaviorSubject(value: 0)
+    
+    let validAgeText = Observable.just("만 17세 이상만 가입 가능합니다.")
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        addSubviews()
+        setConstraints()
+        configureUI()
+        bind()
+    }
+    
+    func bind() {
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                owner.navigationController?.pushViewController(SearchViewController(), animated: true)
+            }
+            .disposed(by: disposeBag)
         
+        birthDayPicker.rx.date.bind(with: self) { owner, date in
+            let component = Calendar.current.dateComponents([.day, .month, .year], from: date)
+            owner.year.accept(component.year ?? 0)
+            owner.month.accept(component.month ?? 0)
+            owner.day.accept(component.day ?? 0)
+        }
+        .disposed(by: disposeBag)
+  
+        year.map { "\($0)년" }
+            .bind(to: yearLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        month.map { "\($0)월" }
+            .bind(to: monthLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        day.map { "\($0)일 "}
+            .bind(to: dayLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        validAgeText.bind(to: infoLabel.rx.text)
+            .disposed(by: disposeBag)
+    }
+    
+    func addSubviews() {
+        view.addSubviews([infoLabel, containerStackView, birthDayPicker, nextButton])
+        containerStackView.addArrangedSubviews([yearLabel, monthLabel, dayLabel])
+    }
+    
+    func setConstraints() {
+        let safeArea = view.safeAreaLayoutGuide
+        
+        infoLabel.snp.makeConstraints {
+            $0.top.equalTo(safeArea).offset(150)
+            $0.centerX.equalToSuperview()
+        }
+        
+        containerStackView.snp.makeConstraints {
+            $0.top.equalTo(infoLabel.snp.bottom).offset(30)
+            $0.centerX.equalToSuperview()
+        }
+        
+        [yearLabel, monthLabel, dayLabel].forEach {
+            $0.snp.makeConstraints {
+                $0.width.equalTo(80)
+            }
+        }
+        
+        birthDayPicker.snp.makeConstraints {
+            $0.top.equalTo(containerStackView.snp.bottom)
+            $0.centerX.equalToSuperview()
+        }
+   
+        nextButton.snp.makeConstraints {
+            $0.height.equalTo(50)
+            $0.top.equalTo(birthDayPicker.snp.bottom).offset(30)
+            $0.horizontalEdges.equalTo(safeArea).inset(20)
+        }
+    }
+    
+    func configureUI() {
         view.backgroundColor = .white
+        
+        // birthDayPicker
+        birthDayPicker.setUI()
+        
+        // containerStackView
+        containerStackView.setUI()
+        
+        // labels
+        [yearLabel, monthLabel, dayLabel].forEach {
+            $0.textAlignment = .center
+            $0.textColor = .black
+            $0.font = .systemFont(ofSize: 18, weight: .regular)
+        }
+        
+        // nextButton
+        nextButton.setUI(title: "가입하기")
     }
 }
